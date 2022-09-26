cfcba0f4e5eb55507f14b99fdc675513b86dd032
Nada Sabry <Nada_201900894@fci.helwan.edu.eg>
2022-09-26 13:41:24 +0200

    layout

---
+++ Components/CategoryMenu.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rosa_Bella.Components
+{
+    public class CategoryMenu : ViewComponent
+    {
+        private readonly ApplicationDbContext db;
+
+        public CategoryMenu(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var categorys = db.MainCategorys.ToList();
+            return View(categorys);
+        }
+    }
+}
